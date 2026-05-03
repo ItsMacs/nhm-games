efd5745451973e5c53e8362b383fb33c75b02112
Alice Benedetti <70864018+ItsMacs@users.noreply.github.com>
2026-05-03 21:56:46 +0200

    Initial commit

---
+++ src/main/java/eu/macsworks/projectnhm/games/nhmGames/NhmGames.java
@@ -0,0 +1,16 @@
+package eu.macsworks.projectnhm.games.nhmGames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NhmGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
